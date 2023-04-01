5323afc2bdc12f3f7bd135a0e67ace29ff5e44d1
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-01 18:30:18 +0530

    Connecting admin page and shop page to the database

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db=require('./util/database');
 
 const errorController = require('./controllers/error');
 
@@ -14,7 +13,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,4 +21,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(4000);
